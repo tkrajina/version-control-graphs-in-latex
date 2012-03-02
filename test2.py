4f7477849a149869264cb3591af4e57cb23251ff
Tomo Krajina <tkrajina@gmail.com>
2012-03-02 21:57:26 +0100

    branch color, arrow color

--- test2.py
+++ test2.py
@@ -7,19 +7,21 @@ NODE_RADIUS = 100
 def row_column_to_coordinates( row, column ):
 	return ( column * ROW_COLUMN_SIZE, row * ROW_COLUMN_SIZE )
 
-def get_latex_point( node, height ):
+def get_latex_point( node, height, color = None ):
 	x, y = row_column_to_coordinates( node.row, node.column )
 
-	color = ( 0, 0, 0 )
+	if not color:
+		color = ( 0, 0, 0 )
 
 	result = '\\put(' + str( x - 50 ) + ',' + str( y + 100 ) + '){\\makebox(0,0)[lb]{\\smash{{\\SetFigFont{12}{14.4}{\\rmdefault}{\\mddefault}{\\updefault}{\\textit{' + node.label + '}}}}}}%\n'
 	result += '{\\color[rgb]{' + str( color[ 0 ] ) + ',' + str( color[ 1 ] ) + ',' + str( color[ 2 ] ) + '}\\put(' + str( x ) + ',' + str( y ) + '){\\circle*{' + str( NODE_RADIUS ) + '}}}%\n'
 
 	return result
 
-def get_latex_arrow( node1, node2, height ):
+def get_latex_arrow( node1, node2, height, color = None ):
 
-	color = ( 0, 0, 0 )
+	if not color:
+		color = ( 0, 0, 0 )
 
 	x1, y1 = row_column_to_coordinates( node1.row, node1.column )
 	#x2, y2 = row_column_to_coordinates( node2.row, node2.column )
@@ -50,8 +52,8 @@ class Node:
 		self.row = 0
 		self.column = 0
 
-	def get_latex_string( self, height ):
-		return get_latex_point( self, height )
+	def get_latex_string( self, height, color = None ):
+		return get_latex_point( self, height, color = color )
 
 	def __str__( self ):
 		return '[node:{0}:{1},{2}]'.format( self.label, self.row, self.column )
@@ -64,11 +66,13 @@ class Branch:
 	nodes = None
 	row = None
 	branch_from = None
+	color = None
 
-	def __init__( self, label = None, nodes = None, row = None, branch_from = None ):
+	def __init__( self, label = None, nodes = None, row = None, branch_from = None, color = None ):
 		self.label = label if label else ''
 		self.row = row if row else 0
 		self.branch_from = branch_from
+		self.color = color if color else ( 0, 0, 0 )
 
 		self.__nodes = []
 
@@ -99,8 +103,11 @@ class Branch:
 			node.row = self.row
 			node.column = start_column + index
 
-	def get_latex_string( self, height ):
+	def get_latex_string( self, height, color = None ):
 		result = ''
+
+		if not color:
+			color = self.color
 
 		if self.branch_from:
 			nodes = [ self.branch_from ] + self.__nodes
@@ -108,12 +115,12 @@ class Branch:
 			nodes = self.__nodes
 
 		for index, node in enumerate( nodes ):
-			result += node.get_latex_string( height )
+			result += node.get_latex_string( height, color = color )
 
 			if index > 0:
 				previous_node = nodes[ index - 1 ]
 
-				result += get_latex_arrow( previous_node, node, height )
+				result += get_latex_arrow( previous_node, node, height, color = self.color )
 
 		return result
 
@@ -134,7 +141,11 @@ class Graph:
 		self.__branches.append( branch )
 		branch.reload_points_positions()
 
-	def add_arrow( self, node1, node2 ):
+	def add_arrow( self, node1, node2, color = None ):
+
+		if not color:
+			color = ( 0, 0, 0 )
+
 		if not isinstance( node1, Node ):
 			node1 = self.find_node( node1 )
 
@@ -147,7 +158,7 @@ class Graph:
 		if not node2:
 			raise Error( 'No node2 for arrow' )
 
-		self.__arrows.append( [ node1, node2 ] )
+		self.__arrows.append( [ node1, node2, color ] )
 
 	def find_node( self, node_label ):
 		for branch in self.__branches:
@@ -183,8 +194,8 @@ class Graph:
 		for branch in self.__branches:
 			result += branch.get_latex_string( height )
 
-		for node1, node2 in self.__arrows:
-			result += get_latex_arrow( node1, node2, height )
+		for node1, node2, color in self.__arrows:
+			result += get_latex_arrow( node1, node2, height, color = color )
 
 		result += '\\end{picture}\n'
 
@@ -204,21 +215,21 @@ if __name__ == '__main__':
 			label = 'eksperiment',
 			row = 2,
 			nodes = 'xyzqw',
+			color = ( 0, 0, 1 ),
 			branch_from = graph.find_node( 'b' ) ) )
 
 	graph.add_branch( Branch(
 			label = 'eksperiment 2',
 			row = 1,
 			nodes = '1234',
+			color = ( 1, 0, 0 ),
 			branch_from = graph.find_node( 'g' ) ) )
 
-	graph.add_arrow( 'd', 'z' )
+	graph.add_arrow( 'd', 'z', color = ( 0, 1, 0 ) )
 	graph.add_arrow( 'q', 'g' )
 
 	# TODO: node color
-	# TODO: branch color
 	# TODO: branch label
-	# TODO: arrow color
 
 	print """\\documentclass[11pt,oneside,a4paper]{report}
 
