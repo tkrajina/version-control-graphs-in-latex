356f62d95cbb08953c49230de682ce90d902b7ba
Tomo Krajina <tkrajina@gmail.com>
2012-03-03 06:13:44 +0100

    New version

--- cvgraphtex/__init__.py
+++ cvgraphtex/__init__.py
@@ -2,25 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import logging as mod_logging
-import math as mod_math
 
-NODE_RADIUS = 150
+ROW_COLUMN_SIZE = 400
+NODE_RADIUS = 100
 
-ROW_COLUMN_SIZE = 500
+def row_column_to_coordinates( row, column ):
+	return ( column * ROW_COLUMN_SIZE, ROW_COLUMN_SIZE + row * ROW_COLUMN_SIZE )
 
-def distance( x1, y1, x2, y2 ):
-	return mod_math.sqrt( ( x2 - x1 ) ** 2 + ( y2 - y1 ) ** 2 )
+def get_latex_point( node, height, color = None ):
+	x, y = row_column_to_coordinates( node.row, node.column )
 
-def get_latex_arrow( node1, node2, left_padding, color = None ):
-	color = color if color else ( 0, 0, 0 )
+	if not color:
+		color = ( 0, 0, 0 )
 
-	x1 = node1.x
-	y1 = node1.y
-	x2 = node2.x
-	y2 = node2.y
+	result = '\\put(' + str( x - 50 ) + ',' + str( y + 100 ) + '){\\makebox(0,0)[lb]{\\smash{{\\SetFigFont{12}{14.4}{\\rmdefault}{\\mddefault}{\\updefault}{\\textit{' + node.label + '}}}}}}%\n'
+	result += '{\\color[rgb]{' + str( color[ 0 ] ) + ',' + str( color[ 1 ] ) + ',' + str( color[ 2 ] ) + '}\\put(' + str( x ) + ',' + str( y ) + '){\\circle*{' + str( NODE_RADIUS ) + '}}}%\n'
 
-	vector_x = ( x2 - x1 ) / ROW_COLUMN_SIZE
-	vector_y = ( y2 - y1 ) / ROW_COLUMN_SIZE
+	return result
+
+def get_latex_arrow( node1, node2, height, color = None ):
+
+	if not color:
+		color = ( 0, 0, 0 )
+
+	x1, y1 = row_column_to_coordinates( node1.row, node1.column )
+	#x2, y2 = row_column_to_coordinates( node2.row, node2.column )
+
+	vector_x = node2.column - node1.column
+	vector_y = node2.row - node1.row
 
 	if vector_x == 0:
 		length = ROW_COLUMN_SIZE * abs( vector_y )
@@ -28,107 +37,98 @@ def get_latex_arrow( node1, node2, left_padding, color = None ):
 		length = ROW_COLUMN_SIZE * abs( vector_x )
 
 	length = length * .9
+	return '\\thicklines{\\color[rgb]{' + str( color[ 0 ] ) + ',' + str( color[ 1 ] ) + ',' + str( color[ 2 ] ) + '}\\put(' + str( x1 ) + ',' + str( y1 ) + '){\\vector(' + str( vector_x ) + ',' + str( vector_y ) + '){' + str( length ) + '}}}%\n'
 
-	return '\\thicklines{\\color[rgb]{' + str( color[ 0 ] ) + ',' + str( color[ 1 ] ) + ',' + str( color[ 2 ] ) + '}\\put(' + str( left_padding + x1 ) + ',' + str( y1 ) + '){\\vector(' + str( vector_x ) + ',' + str( vector_y ) + '){' + str( length ) + '}}}%\n'
+def get_latex_text( row, label ):
+	y = ROW_COLUMN_SIZE + row * ROW_COLUMN_SIZE
+	return '\\put(' + str( 0 ) + ',' + str( y ) + '){\\makebox(0,0)[lb]{\\smash{{\\SetFigFont{12}{14.4}{\\rmdefault}{\\mddefault}{\\updefault}{' + label + '}}}}}%\n'
 
 class Node:
-	
+
 	label = None
 
-	# x and y will be set only at the moment the LaTeX string is built:
-	x = None
-	y = None
+	row = None
+	column = None
 
-	color = None
+	def __init__( self, label ):
+		self.label = label if label else ''
+		self.row = 0
+		self.column = 0
 
-	def __init__( self, label, color = None ):
-		self.label = label
-		self.x = None
-		self.y = None
-
-		self.color = color if color else ( 0, 0, 0, )
-
-	def to_latex_string( self, x, y, left_padding ):
-		self.x = x
-		self.y = y
-
-		mod_logging.debug( 'Node {0} to ({1},{2})'.format( self.label, self.x, self.y ) )
-
-		result = '\\put(' + str( left_padding + x - 50 ) + ',' + str( y + 140 ) + '){\\makebox(0,0)[lb]{\\smash{{\\SetFigFont{12}{14.4}{\\rmdefault}{\\mddefault}{\\updefault}{\\textit{' + self.label + '}}}}}}%\n'
-		result += '{\\color[rgb]{' + str( self.color[ 0 ] ) + ',' + str( self.color[ 1 ] ) + ',' + str( self.color[ 2 ] ) + '}\\put(' + str( left_padding + x ) + ',' + str( y ) + '){\\circle*{' + str( NODE_RADIUS ) + '}}}%\n'
-
-		return result
+	def get_latex_string( self, height, color = None ):
+		return get_latex_point( self, height, color = color )
 
 	def __str__( self ):
-		return '[node:{0},{1}]'.format( self.x, self.y )
+		return '[node:{0}:{1},{2}]'.format( self.label, self.row, self.column )
 
 class Branch:
 
-	title = None
-
 	__nodes = None
 
-	start_node = None
-
+	label = None
+	nodes = None
+	row = None
+	branch_from = None
 	color = None
 
-	def __init__( self, title = None, start_node = None, color = None ):
-		self.title = title
-		self.__nodes = []
-		self.start_node = start_node
+	def __init__( self, label = None, nodes = None, row = None, branch_from = None, color = None ):
+		self.label = label if label else ''
+		self.row = row if row else 0
+		self.branch_from = branch_from
 		self.color = color if color else ( 0, 0, 0 )
+
+		self.__nodes = []
+
+		if nodes:
+			for node in nodes:
+				self.add_node( node )
 
 	def add_node( self, node ):
 		""" Node may be instance of Node or string (note label) """
 		if isinstance( node, Node ):
 			self.__nodes.append( node )
 		else:
-			self.__nodes.append( Node( node, color = self.color ) )
-
-	def add_nodes( self, nodes ):
-		for node in nodes:
-			self.add_node( node )
-
-	def get_node( self, index ):
-		return self.__nodes[ index ]
+			self.__nodes.append( Node( label = str( node ) ) )
 
 	def find_node( self, node_label ):
 		for node in self.__nodes:
 			if node.label == node_label:
 				return node
-
 		return None
 
-	def to_latex_string( self, y, left_padding ):
+	def reload_points_positions( self, column ):
+		start_column = column
+
+		if self.branch_from:
+			start_column = self.branch_from.column + 1
+
+		for index, node in enumerate( self.__nodes ):
+			node.row = self.row
+			node.column = start_column + index
+
+	def get_latex_string( self, height, color = None ):
 		result = ''
 
-		mod_logging.debug( 'Branch starting at {0}'.format( y ) )
+		if not color:
+			color = self.color
 
-		if self.start_node:
-			start_x = self.start_node.x + ROW_COLUMN_SIZE
+		if self.branch_from:
+			nodes = [ self.branch_from ] + self.__nodes
 		else:
-			start_x = 0
-		start_y = y
+			nodes = self.__nodes
 
-		if self.title:
-			result = '\\put(' + str( 0 ) + ',' + str( y ) + '){\\makebox(0,0)[lb]{\\smash{{\\SetFigFont{12}{14.4}{\\rmdefault}{\\mddefault}{\\updefault}{\\textit{' + self.title + '}}}}}}%\n'
+		for index, node in enumerate( nodes ):
+			result += node.get_latex_string( height, color = color )
 
-		# Nodes:
-		for index, node in enumerate( self.__nodes ):
-			result += node.to_latex_string( x = start_x + index * ROW_COLUMN_SIZE, y = start_y, left_padding = left_padding )
+			if index > 0:
+				previous_node = nodes[ index - 1 ]
 
-		#Arrows
-		for index, node in enumerate( self.__nodes ):
-			if index < len( self.__nodes ) - 1:
-				next_node = self.__nodes[ index + 1 ]
-				result += get_latex_arrow( node, next_node, left_padding = left_padding, color = self.color )
-
-		if self.start_node:
-			mod_logging.debug( "start node:" + str( self.start_node ) )
-			mod_logging.debug( "first node:" + str( self.__nodes[ 0 ] ) )
-			result += get_latex_arrow( self.start_node, self.__nodes[ 0 ], left_padding = left_padding, color = self.color )
+				result += get_latex_arrow( previous_node, node, height, color = self.color )
 
 		return result
+
+	def __str__( self ):
+		return '[branch:{0}:{1}]'.format( self.label, self.__nodes )
 
 class Graph:
 
@@ -136,37 +136,60 @@ class Graph:
 
 	__arrows = None
 
-	left_padding = None
+	column = None
 
-	def __init__( self, left_padding = None ):
+	def __init__( self, column ):
 		self.__branches = []
-		self.left_padding = left_padding if left_padding else 0
 		self.__arrows = []
+		self.column = column if column else 0
+
+	def add_branch( self, branch ):
+		if len( self.__branches ) == 0 and not branch.label:
+			branch.label = 'master'
+
+		self.__branches.append( branch )
+		branch.reload_points_positions( self.column )
+
+	def add_arrow( self, node1, node2, color = None ):
+
+		if not color:
+			color = ( 0, 0, 0 )
+
+		if not isinstance( node1, Node ):
+			node1 = self.find_node( node1 )
+
+		if not isinstance( node2, Node ):
+			node2 = self.find_node( node2 )
+
+		if not node1:
+			raise Error( 'No node1 for arrow' )
+
+		if not node2:
+			raise Error( 'No node2 for arrow' )
+
+		self.__arrows.append( [ node1, node2, color ] )
 
 	def find_node( self, node_label ):
 		for branch in self.__branches:
 			node = branch.find_node( node_label )
 			if node:
 				return node
+
 		return None
 
-	def add_branch( self, branch ):
-		self.__branches.append( branch )
+	def get_max_row( self ):
+		result = 0
 
-	def get_branch( self, index ):
-		return self.__branches[ index ]
+		for branch in self.__branches:
+			if branch.row > result:
+				result = branch.row
 
-	def add_arrow( self, node1, node2, color = None ):
-		if not isinstance( node1, Node ):
-			node1 = self.find_node( node1 )
-		if not isinstance( node2, Node ):
-			node2 = self.find_node( node2 )
+		return result
 
-		self.__arrows.append( [ node1, node2, color ] )
-
-	def to_latex_string( self ):
+	def get_latex_string( self ):
 		width = 2000
-		height = len( self.__branches ) * ROW_COLUMN_SIZE
+		height = ( 3 + self.get_max_row() ) * ROW_COLUMN_SIZE 
+		mod_logging.debug( 'height = {0}'.format( height ) )
 
 		result = '\\setlength{\\unitlength}{4144sp}%\n'
 		result += '\\begingroup\\makeatletter\\ifx\\SetFigFont\\undefined%\n'
@@ -177,19 +200,19 @@ class Graph:
 		result += '\\fi\\endgroup%\n'
 		result += '\\begin{picture}(' + str( width ) + ',' + str( height ) + ')(0,0)%\n'
 
-		start_y = height
 
-		for index, branch in enumerate( self.__branches ):
-			result += branch.to_latex_string( y = start_y - index * ROW_COLUMN_SIZE, left_padding = self.left_padding )
+		for branch in self.__branches:
+			result += branch.get_latex_string( height )
 
-		for arrow in self.__arrows:
-			arrow_x, arrow_y, color = arrow
-			mod_logging.debug( 'arrow color: {0}'.format( color ) )
-			result += get_latex_arrow( arrow_x, arrow_y, color = color, left_padding = self.left_padding )
+		for branch in self.__branches:
+			result += get_latex_text( branch.row, branch.label + ':' )
+
+		for node1, node2, color in self.__arrows:
+			result += get_latex_arrow( node1, node2, height, color = color )
 
 		result += '\\end{picture}\n'
 
 		return result
 
-	def get_latex_arrow( self, node1, node2 ):
-		return get_latex_arrow( node1, node2, left_padding = self.left_padding )
+	def __str__( self ):
+		return '[graph:{0}]'.format( self.__branches )
